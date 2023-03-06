d699aac4b0778d17a55c515f46ecdfd5c2ee40df
pmayavi <pmayav@eafit.edu.co>
2023-03-05 23:07:42 -0500

    while true & commands

--- Laboratorio N1-RPC/Back/TransactionService/transaction.cs
+++ Laboratorio N1-RPC/Back/TransactionService/transaction.cs
@@ -23,7 +23,6 @@ public class GreeterService : Greeter.GreeterBase
 
     static void Main(string[] args)
     {
-        // Display the number of command line arguments.
-        Console.WriteLine(args.Length);
+        while (true) { }
     }
 }
