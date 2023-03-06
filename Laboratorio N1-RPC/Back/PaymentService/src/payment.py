44221686c5138f2b3e0b695fca768d683a3c8d70
pmayavi <pmayav@eafit.edu.co>
2023-03-06 11:23:59 -0500

    payment updated

--- Laboratorio N1-RPC/Back/PaymentService/src/payment.py
+++ Laboratorio N1-RPC/Back/PaymentService/src/payment.py
@@ -11,8 +11,19 @@ HOST = '[::]:8080'
 
 class ProductService(Service_pb2_grpc.ProductServiceServicer):
     def CheckItem(self, response, context):
-        bank = response.item
-        return Service_pb2.TransactionResponse(status=1, inventory=100)
+        item = response.item
+        with open("banks.json", "r") as inv:
+            inventory = json.loads(inv.read())
+        if item in inventory:
+            inventory[item] -= 100
+            with open("banks.json", "w") as outfile:
+                json.dump(inventory, outfile, indent=4)
+            print("Request is received: " + item +
+                  " now has " + str(inventory[item]))
+        else:
+            print("Card doesn't exist.")
+            return Service_pb2.TransactionResponse(status=0, inventory=0)
+        return Service_pb2.TransactionResponse(status=1, inventory=inventory[item])
 
     def ChangeItem(self, response, context):
         pass
