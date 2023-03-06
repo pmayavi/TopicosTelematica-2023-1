e09f6d3322938cc1e8c83615f6b8cda913d03375
pmayavi <pmayav@eafit.edu.co>
2023-03-05 21:04:23 -0500

    js product service

--- Laboratorio N1-RPC/Back/OnlineService/src/server.js
+++ Laboratorio N1-RPC/Back/OnlineService/src/server.js
@@ -19,11 +19,37 @@ const packageDefinition = protoLoader.loadSync(
 
 console.info("Consumer service is started...");
 
-const inventoryService = grpc.loadPackageDefinition(packageDefinition).InventoryService;
+function check(client, name) {
+  client.CheckItem({ item: name, num: 0 }, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Response received from remote service:', data); // API response
+    }
+  });
+}
+
+function change(client, name, n) {
+  client.ChangeItem({ item: name, num: n }, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Response received from remote service:', data); // API response
+    }
+  });
+}
+
+const productService = grpc.loadPackageDefinition(packageDefinition).ProductService;
 
 function main() {
-  const client = new inventoryService(REMOTE_HOST, grpc.credentials.createInsecure());
 
+  const client = new productService(REMOTE_HOST, grpc.credentials.createInsecure());
+
+  check(client, "Coffee");
+  check(client, "Pens");
+  change(client, "Pens", 1);
+  check(client, "Pens");
+  change(client, "Pens", -1);
 };
 
 main();
